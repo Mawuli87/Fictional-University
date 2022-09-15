84917620699f9eed216883eda2b8d8ed8f8c4d56
Mawuli87 <yawomessie@gmail.com>
2022-09-15 03:55:33 +0000

    Archive added

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
         
             <ul>
-              <li <?php if(is_page('about-us') or wp_get_post_parent_id(get_the_ID()) == 11) echo 'class = "current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <li <?php if(is_page('about-us') or wp_get_post_parent_id(get_the_ID())) echo 'class = "current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
