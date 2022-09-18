01b346ec8a5968fbdae53e3318de07561dc1650c
Mawuli87 <nyonouglokoffi8@gmail.com>
2022-09-18 16:21:20 +0000

    Displaying custom posts and creation of archive-ent.php

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -2,10 +2,11 @@
 
 function university_post_types() {
   register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
-    'show_in_rest' => true,
     'labels' => array(
       'name' => 'Events',
       'add_new_item' => 'Add New Event',
